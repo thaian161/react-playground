48f76266eecc55ad800ed007b6734bbc828da23b
thaian161 <thaian161@yahoo.com>
2022-10-24 04:46:56 -0500

    create helper function to sepeate inline Date from components

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
       <div>
-        <div>{props.date.toLocaleString('en-US', { month: 'long' })}</div>
-        <div>{props.date.toLocaleString('en-US', { day: '2-digit' })}</div>
-        <div>{props.date.getFullYear()}</div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
       </div>
 
       <div className="expense-item__description">
