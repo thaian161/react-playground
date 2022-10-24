a1d88452b9c0a719b7ef399cc5096f161c687d98
thaian161 <thaian161@yahoo.com>
2022-10-24 04:13:52 -0500

    Allow dynamic data changed using Expression in JSX

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,13 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 8, 22);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 77;
+
   return (
     <div className="expense-item">
-      <div>March 22, 2022</div>
+      <div>{expenseDate.toISOString()}</div>
 
       <div className="expense-item__description">
-        <h2> Insurance</h2>
-        <div className="expense-item__price"> $77 </div>
+        <h2> {expenseTitle}</h2>
+        <div className="expense-item__price"> ${expenseAmount} </div>
       </div>
     </div>
   );
