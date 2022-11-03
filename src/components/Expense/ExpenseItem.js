c87158cd7d610bbed19d532cd35a5f5eb57ea57e
thaian161 <thaian161@yahoo.com>
2022-11-02 20:54:27 -0500

    organize components folder

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,6 +1,6 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI-Wrapper/Card';
 
 const ExpenseItem = (props) => {
   return (
