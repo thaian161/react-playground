ca171a45b2b94c013f5bbc462a718320f65f3307
thaian161 <thaian161@yahoo.com>
2022-11-02 23:00:33 -0500

    example of useState

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,19 +1,29 @@
+import { useState } from 'react';
+
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI-Wrapper/Card';
 
 const ExpenseItem = (props) => {
+
+const [title, setTitle]= useState(props.title);
+
+// function to use with button onClick
+  const clickHandler = ()=>{
+    setTitle("Updated");
+    console.log(title)
+  }
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
 
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
 
-    //button onClick with anonymous functions
-      <button onClick={()=>{console.log("click yay")}}>Change Title</button>
+      <button onClick={clickHandler}>Change Title</button>
 
     </Card>
   );
