ce881acc4fe71df18b1a03f9dd3613ad0b4053b9
thaian161 <thaian161@yahoo.com>
2022-11-17 16:49:58 -0600

    move expenses array out of App

--- src/App.js
+++ src/App.js
@@ -2,33 +2,37 @@ import Expenses from './components/Expense/Expenses';
 import ExpensesFilter from './components/Expense/ExpensesFilter';
 import NewExpense from './components/NewExpense/NewExpense';
 
+import { useState } from 'react';
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Skins Care',
+    amount: 100.79,
+    date: new Date(2022, 7, 14),
+  },
+  {
+    id: 'e2',
+    title: 'Macbook Pro',
+    amount: 3999.99,
+    date: new Date(2022, 2, 12),
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 150,
+    date: new Date(2022, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'Winter Coat',
+    amount: 490,
+    date: new Date(2022, 5, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Skins Care',
-      amount: 100.79,
-      date: new Date(2022, 7, 14),
-    },
-    {
-      id: 'e2',
-      title: 'Macbook Pro',
-      amount: 3999.99,
-      date: new Date(2022, 2, 12),
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 150,
-      date: new Date(2022, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'Winter Coat',
-      amount: 490,
-      date: new Date(2022, 5, 12),
-    },
-  ];
+  
 
   return (
     <div>
